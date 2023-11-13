650dfb270fe3388e75de675b234ff18f19a970da
ViaKatia <113637269+ViaKatia@users.noreply.github.com>
2023-11-13 14:22:40 +0100

    EFC DAO Implementations

---
+++ EfcDataAccess/DAOs/PostEfcDao.cs
@@ -0,0 +1,45 @@
+﻿using Application.DaoInterfaces;
+using Models;
+using Models.DTOs;
+
+
+namespace EfcDataAccess.DAOs;
+
+public class PostEfcDao:IPostDao
+{
+    public Task<Post> CreateAsync(Post post)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Post>> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public Task<IEnumerable<Post>> GetAsync(SearchPostParametersDto searchParameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Post post)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post?> GetByIdAsync(int postId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
+
+
---
+++ EfcDataAccess/DAOs/UserEfcDao.cs
@@ -0,0 +1,33 @@
+﻿using Application.DaoInterfaces;
+using Models;
+using Models.DTOs;
+
+namespace EfcDataAccess.DAOs;
+
+public class UserEfcDao:IUserDao
+{
+    public Task<User> CreateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User?> GetByUsernameAsync(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> GetAsync(SearchUserParametersDto searchParameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
